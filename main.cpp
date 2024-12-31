9559c12bcc47e8c2bd0305aef84c3e1a916166cc
William <111227194+ViliamOleos@users.noreply.github.com>
2024-12-31 13:00:14 +0500

    main.cpp*>@(header creation)
    i drew the rest of the owl

--- main.cpp
+++ main.cpp
@@ -1,9 +1,53 @@
+#include <algorithm>
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
+string headergen(string text, char symbol, unsigned int symcount)
+{
+    string hdstream;
+
+    unsigned int sidebars = (symcount-text.length())/2;
+    for(int i=0; i<sidebars; i++) {hdstream+=symbol;}
+    hdstream += text;
+    for(int i=0; i<sidebars; i++) {hdstream+=symbol;}
+
+    return hdstream;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    //-----------------------VARS-----------------------
+    string txt; char sym; unsigned int length;
+    char erranswer;
+
+
+
+    //----------------------INPUT!----------------------
+    input:
+
+    cout << "Header Text (Pls Use '(' for Space) [s.length()%2=0]:\n"; // Ask
+    cin >> txt; // Receive Answer
+    // ERROR HANDLING \/
+    if(txt.length()%2!=0){cout<<"ERROR: Text had odd amount of characters, trying again...\n\n";goto input;}
+    // ERROR HANDLING /\.
+    std::replace(txt.begin(), txt.end(), '@', ' '); // Turn @ -> Space
+
+    cout << "Bar Symbol (Use '(' for Space) [char]:\n"; // Ask
+    cin >> sym; // Receive Answer
+    if(sym=='@'){sym=' ';} // @ -> Space
+
+    cout << "Bar Length (Including Text) [n%2=0]:\n"; // Ask
+    cin >> length; // Receive Answer
+    // ERROR HANDLING \/
+    if(txt.length()%2!=0){cout<<"ERROR: Number is odd, trying again...\n\n";goto input;}
+    // ERROR HANDLING /\.
+
+
+
+    //----------------------OUTPUT----------------------
+    cout << headergen(txt, sym, length); // Return Header
+
     return 0;
 }
